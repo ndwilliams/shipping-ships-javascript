12e1969357c80b2d247dbff0c9f49edc6207da2d
Nicholas Williams <nicholaswilliams910@gmail.com>
2024-05-27 14:06:08 -0500

    added docks to database, created DockList function to Docks.js and basic HTML for main.js to render DockList

--- scripts/main.js
+++ scripts/main.js
@@ -1,7 +1,15 @@
+import { DockList } from "./Docks.js"
+
 const mainContainer = document.querySelector("#container")
 
 const appplicationHTML = `
 <h1>Shipping Ships</h1>
+<article class="details">
+    <section class="detail--column list detail_cities">
+    <h2>Docks</h2>
+    ${DockList()}
+    </section>
+</article>
 `
 
 mainContainer.innerHTML = appplicationHTML
