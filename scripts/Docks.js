6276955fb96eb9f178d00cfdbb2591c8e27e2ac7
Nicholas Williams <nicholaswilliams910@gmail.com>
2024-05-28 13:45:48 -0500

    algorithm created for implementing dock click event displaying the haulers being unloaded

--- scripts/Docks.js
+++ scripts/Docks.js
@@ -1,7 +1,28 @@
 import { getDocks } from "./database.js"
 
+const docks = getDocks()
+
 export const DockList = () => {
-	const docks = getDocks()
+	document.addEventListener("click", (clickEvent) => {
+		const itemClicked = clickEvent.target
+
+		/*
+                Your job is to design an algorithm and implement it
+            */
+
+		// Was a dock list item clicked?
+		// Get the id of the dock clicked
+		// Initialize an empty array
+		// Iterate through all of the haulers
+		// Does the dockId foreign key match the dock's id?
+		// Append to the array
+		// Is there no hauler at the dock/is the array empty?
+		// Display default message - The Shanghai, China dock is currently unloading nothing
+		// Is there one hauler?
+		// Display message  - The Shanghai, China dock is currently unloading Boaty McBoatface
+		// Is there more than one hauler?
+		// Display the message - The Shanghai, China dock is currently unloading Boaty McBoatface, Seawise Giant
+	})
 
 	let docksHTML = "<ul>"
 
